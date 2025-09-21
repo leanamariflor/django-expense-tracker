f99c9c346364ef344ba4d31e313fab12602c4a41
lleanamariflor <lleanamariflor@gmail.com>
2025-09-21 23:57:11 +0800

    feature(expenses): update html and css

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView  # <-- import this
+from django.views.generic import RedirectView  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('expenses/', include('expenses.urls')),
-    path('', RedirectView.as_view(url='/expenses/')),  # <-- redirect root URL
+    path('', RedirectView.as_view(url='/expenses/')), 
+
 ]
