a9ab9445940524a8729198a5afc36fd110fb951d
lleanamariflor <lleanamariflor@gmail.com>
2025-09-21 23:02:18 +0800

    feature(expenses): setup templates

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Expense(models.Model):
+    CATEGORY_CHOICES = [
+        ('Food', 'Food'),
+        ('Transport', 'Transport'),
+        ('Entertainment', 'Entertainment'),
+        ('Other', 'Other'),
+    ]
+
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.amount}"
--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Expense
+from .forms import ExpenseForm
 
-# Create your views here.
+def expense_list(request):
+    expenses = Expense.objects.all().order_by('-date')
+    total = sum(exp.amount for exp in expenses)
+    return render(request, 'expenses/expense_list.html', {'expenses': expenses, 'total': total})
+
+def add_expense(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('expense_list')
+    else:
+        form = ExpenseForm()
+    return render(request, 'expenses/expense_form.html', {'form': form})
+
+def edit_expense(request, pk):
+    expense = get_object_or_404(Expense, pk=pk)
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST, instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect('expense_list')
+    else:
+        form = ExpenseForm(instance=expense)
+    return render(request, 'expenses/expense_form.html', {'form': form})
+
+def delete_expense(request, pk):
+    expense = get_object_or_404(Expense, pk=pk)
+    if request.method == 'POST':
+        expense.delete()
+        return redirect('expense_list')
+    return render(request, 'expenses/expense_delete.html', {'expense': expense})
